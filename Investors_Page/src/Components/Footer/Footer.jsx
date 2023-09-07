0f66daeda0d329c2a5b396e7cf4a23cd6c5fafb0
Jack Caldwell-Nichols <jackcaldwell-nichols@Jacks-MacBook-Pro.local>
2023-09-07 11:23:02 +0200

    import icons rmvd

--- Investors_Page/src/Components/Footer/Footer.jsx
+++ Investors_Page/src/Components/Footer/Footer.jsx
@@ -1,6 +1,4 @@
 import "./footer.scss";
-import FacebookIcon from "@mui/icons-material/Facebook";
-import InstagramIcon from "@mui/icons-material/Instagram";
 import FP from "../../assets/FP.png";
 import VS from "../../assets/VS.png";
 import { Link } from "react-router-dom";
